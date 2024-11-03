8c299ade71e74b708eb1f5019d4a49fece57c3d9
artemkolodko <temakolodko@gmail.com>
2024-11-03 09:05:57 +0000

    Add netlify redirects file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const appConfig = {
-  apiUrl: 'http://localhost:8085',
+  apiUrl: 'https://pump-fun-backend.fly.dev',
   tokenFactoryAddress: '0x29219697933b7dd45De8e0Ef33f2CDa162913076'
 }
 
+// http://localhost:8085
 // https://pump-fun-backend.fly.dev
