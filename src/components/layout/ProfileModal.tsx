1ee37f59a2705704ccad1b1689527f5c0ef93273
artemkolodko <temakolodko@gmail.com>
2024-11-10 21:28:01 +0000

    Add link to profile

--- src/components/layout/ProfileModal.tsx
+++ src/components/layout/ProfileModal.tsx
@@ -2,6 +2,7 @@ import {Box, Text} from "grommet";
 import {UserAccount} from "../../types.ts";
 import {Button, Typography} from "antd";
 import {useClientData} from "../../providers/DataProvider.tsx";
+import {Link} from "react-router-dom";
 
 export const ProfileModal = (props: {
   user: UserAccount
@@ -12,10 +13,17 @@ export const ProfileModal = (props: {
   const { onDisconnect } = useClientData()
 
   return <Box>
-    <Box>
+    <Box gap={'2px'}>
       <Text>@{user.username}</Text>
+      <Text>
+        <Link to={'/profile/' + user.address} onClick={() => {
+          onClose()
+        }}>
+          Open profile page
+        </Link>
+      </Text>
     </Box>
-    <Box margin={{ top: '8px' }}>
+    <Box margin={{ top: '16px' }}>
       <Box pad={'4px 8px'} border={{ color: 'white' }} round={'6px'}>
         <Typography.Text copyable={true}>
           {user.address}
