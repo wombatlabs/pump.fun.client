07cd4d96a25ef94ca471f79cea2fd9a190d58d07
artemkolodko <temakolodko@gmail.com>
2025-02-27 15:24:55 +0000

    Production config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
   walletConnectProjectId: 'd9582a28e2633cc50849bab7aa9ad668',
-  apiUrl: 'http://localhost:8085',
+  apiUrl: 'https://pump-fun-backend.fly.dev',
   tokenFactoryCompetition: '0xc115aDA811C5c81f1EafcBe5526d5Fcb73B6b40D',
   tokenFactoryBase: '0x7400bE22b1F3fF409E58738E4cF32290f60b7504',
   competitionDuration: 7 * 24 * 60 * 60 * 1000,
@@ -17,6 +17,6 @@ export const getTokenFactoryAddress = (
 }
 
 // apiUrl:
-// http://localhost:8085
-// https://pump-fun-backend.fly.dev
-// https://pump-fun-backend-staging.fly.dev
+// Production: https://pump-fun-backend.fly.dev
+// Local: http://localhost:8085
+// Staging: https://pump-fun-backend-staging.fly.dev
